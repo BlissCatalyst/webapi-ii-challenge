7d470ff84140a8ac3f363613ca79fd117facd930
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-19 15:45:08 -0600

    can successfully get ID specific post.

--- data/dbRouter.js
+++ data/dbRouter.js
@@ -33,6 +33,25 @@ router.get('/', async (req, res) => {
   } catch {
 
   }
+});
+
+// GET A SINGLE POST BY ID
+router.get('/:id', async (req, res) => {
+  try{
+    const post = await db.findById(req.params.id)
+
+    if(post) {
+      res.status(202).json(post);
+    } else {
+      res.status(500).json({
+        error: 'The post information could not be retrieved.'
+      })
+    }
+  } catch {
+    res.status(404).json({
+      message: 'The post with the specified ID does not exist'
+    })
+  }
 })
 
 module.exports = router;
